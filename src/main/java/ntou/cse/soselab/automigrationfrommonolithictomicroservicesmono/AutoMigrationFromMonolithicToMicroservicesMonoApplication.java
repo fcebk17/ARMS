fc0a8c217fb3dbaa01a41f9d4940e8bbc4edeb75
Tzuyu Huang <fcebk17@gmail.com>
2024-10-19 22:56:06 +0800

    feat: create project

---
+++ src/main/java/ntou/cse/soselab/automigrationfrommonolithictomicroservicesmono/AutoMigrationFromMonolithicToMicroservicesMonoApplication.java
@@ -0,0 +1,13 @@
+package ntou.cse.soselab.automigrationfrommonolithictomicroservicesmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMigrationFromMonolithicToMicroservicesMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMigrationFromMonolithicToMicroservicesMonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ntou/cse/soselab/automigrationfrommonolithictomicroservicesmono/AutoMigrationFromMonolithicToMicroservicesMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.cse.soselab.automigrationfrommonolithictomicroservicesmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMigrationFromMonolithicToMicroservicesMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
