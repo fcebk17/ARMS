8ec4fcd4293831e4e31d0e61c5c6a81afe6e42de
Tzuyu <fcebk17@gmail.com>
2024-10-22 22:29:56 +0800

    feat: clone project by numberOfGroups

--- src/main/java/ntou/cse/soselab/automigrationfrommonolithictomicroservicesmono/CloneProject.java
+++ src/main/java/ntou/cse/soselab/automigrationfrommonolithictomicroservicesmono/CloneProject.java
@@ -1,10 +1,14 @@
 package ntou.cse.soselab.automigrationfrommonolithictomicroservicesmono;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import java.io.IOException;
 import java.nio.file.*;
 
 public class CloneProject {
-
     public void copyDirectory(String source, String Dir1) {
         Path sourceDir = Paths.get(source);
         Path targetDir1 = Paths.get(Dir1);
@@ -32,5 +36,25 @@ public class CloneProject {
                 e.printStackTrace();
             }
         });
+    }
+
+    public int getNumberOfGroups(String projectName, String concept) {
+        try (var mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase("demoData");
+            MongoCollection<Document> clusters = database.getCollection("clusters");
+
+            // Query `numberOfGroups` value
+            Document query = new Document("projectName", projectName).append("concept", concept);
+
+            // Query document
+            Document cluster = clusters.find(query).first();
+
+            if (cluster != null && cluster.containsKey("numberOfGroups")) {
+                return cluster.getInteger("numberOfGroups");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
